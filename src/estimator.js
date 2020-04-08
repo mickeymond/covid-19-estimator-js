ed6fa8645f23087a1c5817eb671f27f957a26b30
Michael Hammond <mickeymond@gmail.com>
2020-04-08 10:32:02 +0000

    Challenge 1 Submission #1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,26 @@
-const covid19ImpactEstimator = (data) => data;
+import {
+  calculateCurrentlyInfected,
+  calculateInfectionsByRequestedTime
+} from './helpers';
+
+const covid19ImpactEstimator = (data) => {
+  // Destructure Data
+  const {
+    reportedCases
+  } = data;
+
+  // Return Results
+  return {
+    data,
+    impact: {
+      currentlyInfected: calculateCurrentlyInfected(reportedCases, 10),
+      infectedByRequestedTime: calculateInfectionsByRequestedTime(reportedCases, 10)
+    },
+    severeImpact: {
+      currentlyInfected: calculateCurrentlyInfected(reportedCases, 50),
+      infectedByRequestedTime: calculateInfectionsByRequestedTime(reportedCases, 50)
+    }
+  }
+};
 
 export default covid19ImpactEstimator;
---
+++ src/helpers.js
@@ -0,0 +1,5 @@
+// Currently Infected Calculator
+export const calculateCurrentlyInfected = (reportedCases, multiplier) => reportedCases * multiplier;
+
+// Infections By Requested Time Calculator
+export const calculateInfectionsByRequestedTime = (reportedCases, multiplier) => calculateCurrentlyInfected(reportedCases, multiplier) * 1024;
