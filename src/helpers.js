acb77bda483139bbd61b9faff2838a4a4c77e2a8
Michael Hammond <mickeymond@gmail.com>
2020-04-08 12:32:26 +0000

    Challenge 1 Submission #11

--- src/helpers.js
+++ src/helpers.js
@@ -5,12 +5,17 @@ export const currentlyInfected = (cases, mul) => cases * mul;
 export const infectionsByRequestedTimeFactor = (periodType, period) => {
   if (periodType === 'days') {
     return period / 3;
-  } else if (periodType === 'weeks') {
-    return period * 7 / 3
-  } else {
-    return period * 7 * 4 / 3
   }
+
+  if (periodType === 'weeks') {
+    return (period * 7) / 3;
+  }
+
+  return (period * 7 * 4) / 3;
 };
 
 // Infections By Requested Time Calculator
-export const infectionsByRequestedTime = (cases, mul, fac) => currentlyInfected(cases, mul) * 2 ** fac;
+export const infectionsByRequestedTime = (cases, mul, fac) => {
+  const result = currentlyInfected(cases, mul);
+  return result * 2 ** fac;
+};
