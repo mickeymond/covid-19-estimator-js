3700107998574124254677bf13d967cabcc2758b
Michael Hammond <mickeymond@gmail.com>
2020-04-10 16:38:00 +0000

    Challenge 3 Submission #1

--- src/estimator.js
+++ src/estimator.js
@@ -15,11 +15,14 @@ const covid19ImpactEstimator = (data) => {
     region: { avgDailyIncomePopulation, avgDailyIncomeInUSD }
   } = data;
 
+  // Calculate Days
+  const days = _CTD(periodType, timeToElapse);
+
   // Calculate Infections By Requested Time Factor
-  const ibrtFactor = Math.trunc(_CTD(periodType, timeToElapse) / 3);
+  const daysFactor = Math.trunc(days / 3);
 
   // Calculate Dollars In Flight Factor
-  const difFactor = avgDailyIncomePopulation * avgDailyIncomeInUSD * _CTD(periodType, timeToElapse);
+  const incomeFactor = avgDailyIncomePopulation * avgDailyIncomeInUSD;
 
   // 35% of Total Hospital Beds
   const xbeds = whatIs('35%').of(totalHospitalBeds);
@@ -29,21 +32,21 @@ const covid19ImpactEstimator = (data) => {
     data,
     impact: {
       currentlyInfected: _CI(reportedCases, 10),
-      infectionsByRequestedTime: _IBRT(reportedCases, 10, ibrtFactor),
-      severeCasesByRequestedTime: Math.trunc(whatIs('15%').of(_IBRT(reportedCases, 10, ibrtFactor))),
-      hospitalBedsByRequestedTime: Math.trunc(xbeds - whatIs('15%').of(_IBRT(reportedCases, 10, ibrtFactor))),
-      casesForICUByRequestedTime: Math.trunc(whatIs('5%').of(_IBRT(reportedCases, 10, ibrtFactor))),
-      casesForVentilatorsByRequestedTime: Math.trunc(whatIs('2%').of(_IBRT(reportedCases, 10, ibrtFactor))),
-      dollarsInFlight: Math.trunc(_IBRT(reportedCases, 10, ibrtFactor) * difFactor)
+      infectionsByRequestedTime: _IBRT(reportedCases, 10, daysFactor),
+      severeCasesByRequestedTime: Math.trunc(whatIs('15%').of(_IBRT(reportedCases, 10, daysFactor))),
+      hospitalBedsByRequestedTime: Math.trunc(xbeds - whatIs('15%').of(_IBRT(reportedCases, 10, daysFactor))),
+      casesForICUByRequestedTime: Math.trunc(whatIs('5%').of(_IBRT(reportedCases, 10, daysFactor))),
+      casesForVentilatorsByRequestedTime: Math.trunc(whatIs('2%').of(_IBRT(reportedCases, 10, daysFactor))),
+      dollarsInFlight: Math.trunc((_IBRT(reportedCases, 10, daysFactor) * incomeFactor) / days)
     },
     severeImpact: {
       currentlyInfected: _CI(reportedCases, 50),
-      infectionsByRequestedTime: _IBRT(reportedCases, 50, ibrtFactor),
-      severeCasesByRequestedTime: Math.trunc(whatIs('15%').of(_IBRT(reportedCases, 50, ibrtFactor))),
-      hospitalBedsByRequestedTime: Math.trunc(xbeds - whatIs('15%').of(_IBRT(reportedCases, 50, ibrtFactor))),
-      casesForICUByRequestedTime: Math.trunc(whatIs('5%').of(_IBRT(reportedCases, 50, ibrtFactor))),
-      casesForVentilatorsByRequestedTime: Math.trunc(whatIs('2%').of(_IBRT(reportedCases, 50, ibrtFactor))),
-      dollarsInFlight: Math.trunc(_IBRT(reportedCases, 50, ibrtFactor) * difFactor)
+      infectionsByRequestedTime: _IBRT(reportedCases, 50, daysFactor),
+      severeCasesByRequestedTime: Math.trunc(whatIs('15%').of(_IBRT(reportedCases, 50, daysFactor))),
+      hospitalBedsByRequestedTime: Math.trunc(xbeds - whatIs('15%').of(_IBRT(reportedCases, 50, daysFactor))),
+      casesForICUByRequestedTime: Math.trunc(whatIs('5%').of(_IBRT(reportedCases, 50, daysFactor))),
+      casesForVentilatorsByRequestedTime: Math.trunc(whatIs('2%').of(_IBRT(reportedCases, 50, daysFactor))),
+      dollarsInFlight: Math.trunc((_IBRT(reportedCases, 50, daysFactor) * incomeFactor) / days)
     }
   };
 };
