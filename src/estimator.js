e6b2105a69f9b4383b02f2d8c021132e9abd40dd
Michael Hammond <mickeymond@gmail.com>
2020-04-10 16:13:14 +0000

    Challenge 2 Submission #8

--- src/estimator.js
+++ src/estimator.js
@@ -21,26 +21,29 @@ const covid19ImpactEstimator = (data) => {
   // Calculate Dollars In Flight Factor
   const difFactor = avgDailyIncomeInUSD * _CTD(periodType, timeToElapse);
 
+  // 35% of Total Hospital Beds
+  const xbeds = whatIs('35%').of(totalHospitalBeds);
+
   // Return Results
   return {
     data,
     impact: {
       currentlyInfected: _CI(reportedCases, 10),
       infectionsByRequestedTime: _IBRT(reportedCases, 10, ibrtFactor),
-      severeCasesByRequestedTime: whatIs('15%').of(_IBRT(reportedCases, 10, ibrtFactor)),
-      hospitalBedsByRequestedTime: whatIs('35%').of(totalHospitalBeds),
-      casesForICUByRequestedTime: whatIs('5%').of(_IBRT(reportedCases, 10, ibrtFactor)),
-      casesForVentilatorsByRequestedTime: whatIs('2%').of(_IBRT(reportedCases, 10, ibrtFactor)),
-      dollarsInFlight: _IBRT(reportedCases, 10, ibrtFactor) * difFactor
+      severeCasesByRequestedTime: Math.trunc(whatIs('15%').of(_IBRT(reportedCases, 10, ibrtFactor))),
+      hospitalBedsByRequestedTime: Math.trunc(xbeds - whatIs('15%').of(_IBRT(reportedCases, 10, ibrtFactor))),
+      casesForICUByRequestedTime: Math.trunc(whatIs('5%').of(_IBRT(reportedCases, 10, ibrtFactor))),
+      casesForVentilatorsByRequestedTime: Math.trunc(whatIs('2%').of(_IBRT(reportedCases, 10, ibrtFactor))),
+      dollarsInFlight: Math.trunc(_IBRT(reportedCases, 10, ibrtFactor) * difFactor)
     },
     severeImpact: {
       currentlyInfected: _CI(reportedCases, 50),
       infectionsByRequestedTime: _IBRT(reportedCases, 50, ibrtFactor),
-      severeCasesByRequestedTime: whatIs('15%').of(_IBRT(reportedCases, 50, ibrtFactor)),
-      hospitalBedsByRequestedTime: whatIs('35%').of(totalHospitalBeds),
-      casesForICUByRequestedTime: whatIs('5%').of(_IBRT(reportedCases, 50, ibrtFactor)),
-      casesForVentilatorsByRequestedTime: whatIs('2%').of(_IBRT(reportedCases, 50, ibrtFactor)),
-      dollarsInFlight: _IBRT(reportedCases, 50, ibrtFactor) * difFactor
+      severeCasesByRequestedTime: Math.trunc(whatIs('15%').of(_IBRT(reportedCases, 50, ibrtFactor))),
+      hospitalBedsByRequestedTime: Math.trunc(xbeds - whatIs('15%').of(_IBRT(reportedCases, 50, ibrtFactor))),
+      casesForICUByRequestedTime: Math.trunc(whatIs('5%').of(_IBRT(reportedCases, 50, ibrtFactor))),
+      casesForVentilatorsByRequestedTime: Math.trunc(whatIs('2%').of(_IBRT(reportedCases, 50, ibrtFactor))),
+      dollarsInFlight: Math.trunc(_IBRT(reportedCases, 50, ibrtFactor) * difFactor)
     }
   };
 };
